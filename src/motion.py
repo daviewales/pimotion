5a9be35d216badec1fc40789e9d3dafec515bd34
David Wales <daviewales@gmail.com>
2014-06-05 03:56:11 +1000

    Add threading. Tweak a bunch of related files.

--- src/motion.py
+++ src/motion.py
@@ -1,15 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Python standard library imports
 import json
-import datetime
+import time
+import queue
+import threading
 
 # Local imports
 import backend
 import postdata
 
 default_settings = {'url':'',
-                    'apikey':''}
+                    'apikey':'',
+                    'tile':20,
+                    'packet_interval':5}
 
 def write_settings(settings=default_settings):
     with open('settings.json', 'w') as file:
@@ -20,35 +24,58 @@ def get_settings(settings_file='settings.json', settings=default_settings):
         local_settings = json.load(file)
 
     settings.update(local_settings)
-
     return settings
 
-def main():
 
-    settings = get_settings(settings_file='settings.json')
-    motion_generator = backend.get_motion_data()
+def collate_and_send_data(data_queue, settings, running):
+    packet = {'apikey': settings['apikey'],
+              'tile': settings['tile'],
+              'frames': []}
+    packet_interval = settings['packet_interval']
+    url = settings['url']
 
-    while True:
-        motion = next(motion_generator)
-        if motion:
-            current_time = datetime.datetime.now()
-            iso_time = current_time - datetime.timedelta(
-                    microseconds=current_time.microsecond)
-            iso_time = str(iso_time)
+    send_time = time.time() + packet_interval
+    while running.is_set():
+        try:
+            data = data_queue.get(timeout=0.1)
+            packet['frames'].append(data)
+        except queue.Empty:
+            data = None
+            pass
 
-            data = {'apikey': settings['apikey'],
-                    'tile': settings['tile'],
-                    'frames': [{'captured': iso_time,
-                                'movements': motion}]
-                   }
-
-            response = postdata.post_json(settings['url'], data)
-            print(json.dumps(data, sort_keys=False, indent=4))
+        if packet['frames'] and time.time() >= send_time: 
+            print(json.dumps(packet, sort_keys=False, indent=4))
+            response = postdata.post_json(url, packet)
             print(response)
+            packet['frames'] = []
+            send_time = time.time() + packet_interval
+
+
+def main():
+    try:
+        settings = get_settings(settings_file='settings.json')
+        data_queue = queue.Queue()
+        running = threading.Event()
+        running.set()
+        send_data_thread = threading.Thread(target=collate_and_send_data,
+                                            args=(data_queue, settings,
+                                                  running))
+        send_data_thread.start()
+
+        for motion in backend.get_motion_data():
+            if motion:
+                data = {'captured': time.strftime('%Y-%m-%d %H:%M:%S'),
+                        'movements': motion}
+                data_queue.put(data)
+
+
+    except KeyboardInterrupt:
+        running.clear()
+        print('\nWaiting for threads to exit...')
+        print('\nGoodbye!')
+    finally:
+        running.clear()
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('\nGoodbye!')
+    main()
